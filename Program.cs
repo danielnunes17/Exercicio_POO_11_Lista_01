eb839efcc3415cc145838665a24d3082ab3351e6
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-01 15:06:22 -0300

    a

--- Program.cs
+++ Program.cs
@@ -20,7 +20,6 @@ for (int i = 1; i <= employeesN; i++)
 }
 Console.Write("Enter the employee id that will have salary increase: ");
 int searchId = int.Parse(Console.ReadLine());
-
 Employee employee = list.Find(x => x.Id == searchId);
 if (employee != null)
 {
