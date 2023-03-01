588ddb69a86b855c4a2c8e1f4f0c6871e89d58ce
Daniel Stephanio Nunes <dti584@localiza.com>
2023-03-01 15:05:07 -0300

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace Exercicio_POO_11_Lista_01
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double porcent)
+        {
+            Salary += Salary * porcent / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id +
+                    ", " +
+                   "Name: " + Name +
+                   ", " +
+                   "Salary: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Exercicio_POO_11_Lista_01;
+using System.Globalization;
+
+Console.Write("How many employees will be registered? ");
+int employeesN = int.Parse(Console.ReadLine());
+
+List<Employee> list = new List<Employee>();
+
+for (int i = 1; i <= employeesN; i++)
+{
+    Console.WriteLine("Emplyoee #" + i + ":");
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    list.Add(new Employee(id, name, salary));
+    Console.WriteLine();
+}
+Console.Write("Enter the employee id that will have salary increase: ");
+int searchId = int.Parse(Console.ReadLine());
+
+Employee employee = list.Find(x => x.Id == searchId);
+if (employee != null)
+{
+    Console.Write("Enter the percentage: ");
+    double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    employee.IncreaseSalary(percent);
+}
+else
+{
+    Console.WriteLine("This id does not exist!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated list of employees:");
+foreach (Employee obj in list)
+    Console.WriteLine(obj);
+
+
